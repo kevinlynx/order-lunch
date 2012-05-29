e6507f3df1dc57f81c1f9c3b0c4fa58f7245548f
kevinlynx <zmhn320@163.com>
2012-05-29 15:39:38 +0800

    fix the system start/stop auth

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,19 +18,25 @@ class HomeController < ApplicationController
   end
 
   def system
-    s = params[:flag].to_i
-    tip = ""
-    if s > 0 
-      User.clear_orders
-      SystemStatus.start
-      tip = t "tip.start_success"
+    if user_signed_in? and current_user.admin?
+      s = params[:flag].to_i
+      tip = ""
+      if s > 0 
+        User.clear_orders
+        SystemStatus.start
+        tip = t "tip.start_success"
+      else
+        User.commit_orders
+        SystemStatus.stop
+        tip = t "tip.stop_success"
+      end
+      respond_to do |format|
+        format.html { redirect_to root_path, :notice => tip }
+      end
     else
-      User.commit_orders
-      SystemStatus.stop
-      tip = t "tip.stop_success"
-    end
-    respond_to do |format|
-      format.html { redirect_to root_path, :notice => tip }
+      respond_to do |format|
+        format.html { redirect_to root_path, :notice => "you're not admin!" }
+      end
     end
   end
 end
